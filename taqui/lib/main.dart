4644cd842173cf822e35dd3dddac2b7c84f88aef
Ana Julia Terra <ajtalves@hotmail.com>
2021-02-22 18:57:04 -0300

    Teste no arquivo main
    tentando tirar o selo de modo debug

--- taqui/lib/main.dart
+++ taqui/lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Login(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
