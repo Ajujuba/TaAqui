c2909916f2f05da894d678012ed14a6d9f7d07f8
Ana Julia Terra <ajtalves@hotmail.com>
2021-03-16 13:06:46 -0300

    Task ET-11 - Completa

--- taqui/lib/screens/tela_perfil_usuario.dart
+++ taqui/lib/screens/tela_perfil_usuario.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 
 const _tituloAppBar = 'Perfil do usuário';
@@ -44,6 +45,18 @@ class PerfilUsuarioState extends State<PerfilUsuario>{
     });
   }
 
+  //função para acessar o Storage e salvar a foto de perfil
+  Future salvarFoto(BuildContext context) async{
+    String fileName = basename(imagem.path); //pegando apenas o nome da img e não o caminho inteiro
+    StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child(fileName); //obtem referencia ao nome do arquivo
+    StorageUploadTask uploadTask = firebaseStorageRef.child("fotos_perfil/").putFile(imagem); // inserindo o arquivo no firebase
+    StorageTaskSnapshot taskSnapshot=await uploadTask.onComplete;
+    setState(() {
+      print("Foto de perfil atualizada");
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Foto de perfil atualizada')));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +71,7 @@ class PerfilUsuarioState extends State<PerfilUsuario>{
             children: <Widget>[
               Container( // define o retangulo laranja onde a foto vai aparecer
                 width: MediaQuery.of(context).size.width,
-                height: 220,
+                height: 270,
                 decoration: BoxDecoration( // define que a cor é laranja
                   color: laranja,
                   borderRadius: BorderRadius.only( // define bordas inferiores arredondadas
@@ -68,11 +81,11 @@ class PerfilUsuarioState extends State<PerfilUsuario>{
                 ),
                 child: Column(
                     children: <Widget>[
-                      Row(
+                      Row( // linha onde estão os elementos do container
                       mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(top: 60.0),
+                          Padding( // define icone e função da galeria
+                            padding: EdgeInsets.only(top: 120.0),
                             child: IconButton(
                               icon: Icon(
                                 Icons.image,
@@ -84,28 +97,28 @@ class PerfilUsuarioState extends State<PerfilUsuario>{
                               },
                             ),
                           ),
-                          Align(
+                          Align( // posicionamento/layout da img de perfil
                             alignment: Alignment.center,
-                            child: CircleAvatar(
+                            child: CircleAvatar( // define a borda da img
                               radius: 100,
                               backgroundColor: Colors.white,
-                              child: ClipOval(
-                                child: new SizedBox(
+                              child: ClipOval( // define onde a img aparece
+                                child: new SizedBox( //define o tamanho da img
                                   width: 180.0,
                                   height: 180.0,
-                                  child: (imagem!=null)?Image.file(
-                                    imagem,
+                                  child: (imagem!=null)?Image.file( //se a pessoa escolher um arquivo
+                                    imagem, //o arquivo será exibido no perfil
                                     fit: BoxFit.fill,
                                   ):Image.network(
                                     "https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                                    fit: BoxFit.fill,
+                                    fit: BoxFit.fill, //senao, uma img default irá ser exibida
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 60.0),
+                          Padding( // define icone e função  da camera
+                            padding: EdgeInsets.only(top: 120.0),
                             child: IconButton(
                               icon: Icon(
                                 Icons.camera_alt,
@@ -120,12 +133,101 @@ class PerfilUsuarioState extends State<PerfilUsuario>{
                         ],
                       ),
                       SizedBox(
-                        height: 20.0,
+                        height: 10.0,
                       ),
-
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RaisedButton(
+                            color: Colors.white,
+                            onPressed: () {
+                              salvarFoto(context);
+                            },
+                            elevation: 0.0,
+                            splashColor: Colors.deepOrange[100],
+                            child: Text(
+                              'Salvar Foto',
+                               style: TextStyle(color: laranja, fontSize: 16.0),
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      padding: EdgeInsets.only(top: 40, bottom: 10),
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        decoration: BoxDecoration(
+                          color: laranja,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(8),
+                          ),
+                        ),
+                        child: SizedBox(
+                          child: FlatButton(
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  "Editar dados pessoais",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 19.0,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                            //onPressed: //PREENCHER ,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 40,),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text('Sair',
+                                  style: TextStyle(
+                                      color: laranja,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 40,),
+                    child:  Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      child: Icon(
+                        Icons.highlight_off,
+                        color:laranja,
+                      ),
+                    ),
+                  ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
