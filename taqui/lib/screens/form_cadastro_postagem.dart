4439a4850448459ac80e52c7059ff4df87daea50
Thaina Mendonça <thaina.mend1@gmail.com>
2021-04-12 21:49:59 -0300

    Telas de postagens

---
+++ taqui/lib/screens/form_cadastro_postagem.dart
@@ -0,0 +1,332 @@
+//import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:taqui/screens/tela_objeto_detalhe.dart';
+
+const _tituloAppBar = 'Cadastro de Postagem';
+
+class CadastroPostagem extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return CadastroPostagemState();
+  }
+}
+
+class CadastroPostagemState extends State<CadastroPostagem> {
+  GlobalKey<FormState> _key = new GlobalKey(); // chave
+  bool _validate = false;
+
+  final picker = ImagePicker();
+
+  File _imagem1 = null;
+  File _imagem2 = null;
+  File _imagem3 = null;
+
+  List<File> _imagens = [];
+
+  //função para acessar as mídias da galeria
+  Future _pegarImgGaleria(int numeroImagem) async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        switch(numeroImagem){
+          case 1:
+            this._imagem1 = File(pickedFile.path);
+            break;
+          case 2:
+            this._imagem2 = File(pickedFile.path);
+            break;
+          case 3:
+            this._imagem3 = File(pickedFile.path);
+            break;
+        }
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+  void _removeImage(int numeroImagem){
+    setState(() {
+      switch(numeroImagem){
+        case 1:
+          this._imagem1 = null;
+          break;
+        case 2:
+          this._imagem2 = null;
+          break;
+        case 3:
+          this._imagem3 = null;
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( // define um titulo pra tela
+        title: Text(_tituloAppBar),
+      ),
+      body: Container( // inseri o container para delimitar o posicioamento dos widgets na tela
+        padding: EdgeInsets.only(
+          top: 20,
+          left: 10,
+          right: 10,
+          bottom: 20,
+        ),
+        color: Colors.white,
+        child: Form(
+          key: _key,
+          child: _formPostagem(),
+        ),
+      ),
+    );
+    }
+
+    Widget _formPostagem(){
+      return ListView( // inseri um ListView para permitir scroll na tela
+        children: <Widget>[
+          TextFormField( // Bloco referente ao input da localização
+            keyboardType: TextInputType.text,
+            onSaved: (String val) { },
+            decoration: InputDecoration(
+              labelText: "Localização",
+              icon: Icon(Icons.zoom_in),
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontSize: 18.0,
+              ),
+            ),
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+              child: TextFormField( // Bloco referente ao input da descrição
+                keyboardType: TextInputType.text,
+                onSaved: (String val) { },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 150),
+                  labelText: "Descrição do objeto...",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontSize: 18.0,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              )
+          ),
+          SizedBox(
+            height: 5,
+          ),
+
+         // Adicionar container de imagens
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey.withOpacity(0.5),
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: this._imagem1 == null ?
+                    GestureDetector(
+                      onTap: (){
+                        _pegarImgGaleria(1);
+                      },
+                      child: Icon(Icons.add, color: Colors.deepOrange),
+                    )
+                        : Stack(
+                      children: <Widget>[
+                        Container(
+                          width: 180,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: FileImage(this._imagem1),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 5,
+                          right: 10,
+                          child: Container(
+                              width: 25,
+                              height: 25,
+                              decoration: BoxDecoration(
+                                color: Colors.deepOrange,
+                                shape: BoxShape.circle,
+                              ),
+                              child: GestureDetector(
+                                onTap: (){
+                                  _removeImage(1);
+                                },
+                                child: Icon(Icons.delete_forever_outlined, color: Colors.white,),
+                              )
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+                Container(
+                  //color: Colors.deepOrange,
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: this._imagem2 == null ?
+                  GestureDetector(
+                    onTap: (){
+                      _pegarImgGaleria(2);
+                    },
+                    child: Icon(Icons.add, color: Colors.deepOrange),
+                  )
+                      : Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 180,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(this._imagem2),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 5,
+                        right: 10,
+                        child: Container(
+                            width: 25,
+                            height: 25,
+                            decoration: BoxDecoration(
+                              color: Colors.deepOrange,
+                              shape: BoxShape.circle,
+                            ),
+                            child: GestureDetector(
+                              onTap: (){
+                                _removeImage(2);
+                              },
+                              child: Icon(Icons.delete_forever_outlined, color: Colors.white,),
+                            )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  //color: Colors.deepOrange,
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: this._imagem3 == null ?
+                  GestureDetector(
+                    onTap: (){
+                      _pegarImgGaleria(3);
+                    },
+                    child: Icon(Icons.add, color: Colors.deepOrange),
+                  )
+                      : Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 180,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(this._imagem3),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 5,
+                        right: 10,
+                        child: Container(
+                            width: 25,
+                            height: 25,
+                            decoration: BoxDecoration(
+                              color: Colors.deepOrange,
+                              shape: BoxShape.circle,
+                            ),
+                            child: GestureDetector(
+                              onTap: (){
+                                _removeImage(3);
+                              },
+                              child: Icon(Icons.delete_forever_outlined, color: Colors.white,),
+                            )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(
+            height: 5,
+          ),
+
+          Container( // container que define o fundo do botão
+            height: 50,
+            alignment: Alignment.centerRight,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.3,1],
+                colors: [
+                  Color(0xFFF58524), //decidir cores posteriormente
+                  Color(0xFFF92B7F),
+                ],
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+            ),
+            child: SizedBox.expand(
+              child:TextButton(
+                child: Container(
+                  child:
+                  Text('Cadastrar',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                onPressed: () { },
+                style: TextButton.styleFrom(
+                  primary: Colors.black,
+                  backgroundColor: Colors.lightBlue,
+                  side: BorderSide(color: Colors.grey, width: 2),
+                ),)
+            ),
+          ),
+        ],
+      );
+    }
+}
---
+++ taqui/lib/screens/tela_postagens.dart
@@ -0,0 +1,113 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:taqui/models/ObjetoPerdido.dart';
+import 'package:taqui/screens/form_cadastro_postagem.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:taqui/screens/tela_objeto_detalhe.dart';
+
+
+const _tituloAppBar = 'Postagens do Usuario';
+
+class PostagensUsuario extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return PostagensUsuarioState();
+  }
+}
+
+class PostagensUsuarioState extends State<PostagensUsuario> {
+  final laranja = Colors.deepOrange;
+
+  void _novapostagem(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => CadastroPostagem()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<int> posts = [for (var i = 1; i <= 23; i++) i]; // Resultados do DB
+    return Scaffold(
+      appBar: AppBar( // define um titulo pra tela
+        title: Text(_tituloAppBar),
+        elevation: 0,
+      ),
+      body: Builder(
+          builder: (context) =>
+              Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Align(
+                      alignment: Alignment.center,
+                        child: Text(
+                          'Minhas postagens',
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .headline4,
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: ListView.builder(
+                            itemCount: posts.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Column(
+                                children: [
+                                    Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                    ConstrainedBox(constraints: BoxConstraints.tightFor(width: 250, height: 40),
+                                      child: ElevatedButton.icon(
+                                          onPressed: () {
+                                            ObjetoPerdido objeto = ObjetoPerdido("Endereço da Postagem ${posts[index]}", "Descrição da Postagem ${posts[index]}");
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(builder: (context) => ObjetoDetalhe(objeto))
+                                            );
+                                          },
+                                          icon: Text('Postagem ${posts[index]}',
+                                              style: TextStyle(color: Colors.black)
+                                          ),
+                                          label: Icon(
+                                            Icons.edit,
+                                            color: Colors.black,
+                                          )
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                  SizedBox(height: 10)
+                                ]
+                              );
+                            },
+                          ),
+                        )
+                      ),
+                      Container(
+                        height: 50.0,
+                        width: 300.0,
+                        margin: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                            color: laranja,
+                            borderRadius: BorderRadius.all(Radius.circular(15))
+                        ),
+                        child:
+                          TextButton(
+                              child: Container(
+                                child:
+                                  Text('Criar nova postagem',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                              ),
+                              onPressed: () { _novapostagem(context); },
+                              style: TextButton.styleFrom(
+                                  primary: Colors.black,
+                                  backgroundColor: Colors.lightBlue,
+                                  side: BorderSide(color: Colors.grey, width: 2),
+                              ),),)]))));}}
