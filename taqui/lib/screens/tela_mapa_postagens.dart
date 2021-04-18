0a3c2c1fb513dd2acbc70ed17ae6c50875321bc4
NicolasOCruz <68904912+NicolasOCruz@users.noreply.github.com>
2021-04-18 14:31:36 -0300

    Dependência Geolocator e Localização Atual do usuário

--- taqui/lib/screens/tela_mapa_postagens.dart
+++ taqui/lib/screens/tela_mapa_postagens.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapaPostagens extends StatefulWidget {
   @override
@@ -12,11 +13,57 @@ class _MapaPostagensState extends State<MapaPostagens> {
   Completer<GoogleMapController> _controller = Completer();
   CameraPosition _posicaoCamera = CameraPosition(
       target: LatLng(-23.5639999, -46.653256),
-      zoom: 18
+      zoom: 16
   );
 
   _onMapCreated(GoogleMapController controller){
     _controller.complete(controller);
+  }
+
+  _adicionarListenerLocalizacao(){
+    var geolocator = Geolocator();
+    var locationOptions = LocationOptions(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    );
+    geolocator.getPositionStream(locationOptions).listen((Position position) {
+      _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+      );
+      _movimentarCamera(_posicaoCamera);
+    });
+  }
+
+  _recuperarUltimaLocalizacaoConhecida() async {
+    Position position = await Geolocator()
+        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      if(position != null){
+        _posicaoCamera = CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 19
+        );
+        _movimentarCamera(_posicaoCamera);
+      }
+    });
+  }
+
+  _movimentarCamera(CameraPosition cameraPosition) async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+          cameraPosition
+      )
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarUltimaLocalizacaoConhecida();
+    _adicionarListenerLocalizacao();
   }
 
   @override
@@ -31,6 +78,8 @@ class _MapaPostagensState extends State<MapaPostagens> {
           mapType: MapType.normal,
           initialCameraPosition: _posicaoCamera,
           onMapCreated: _onMapCreated,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
           //onLongPress: _adicionarMarcador,
           //myLocationButtonEnabled: true,
         ),
